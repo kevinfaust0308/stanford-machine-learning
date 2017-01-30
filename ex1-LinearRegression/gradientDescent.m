cdd6c969927b6c6b46b3c6f9f04dcd2accacc40a
Kevin Faust <ikevinfaust@gmail.com>
2017-01-29 20:28:42 -0500

    Submitted Assignment Solutions

--- ex1-LinearRegression/gradientDescent.m
+++ ex1-LinearRegression/gradientDescent.m
@@ -4,10 +4,7 @@ function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
 %   taking num_iters gradient steps with learning rate alpha
 
 % Initialize some useful values
-% size of X in (rows,columns)
-s = size(X);
-m = s(1);
-n = s(2);
+m = length(y);
 
 J_history = zeros(num_iters, 1);
 
@@ -20,32 +17,26 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-
-    descent_vals = zeros(n,1);
     
-    % this is a real value 
+    % mxn+1 * n+1x1 - mx1 = mx1 matrix 
+    % mx1 matrix containing the difference between our predicted value and real value
     pred_subtract_real = X*theta - y;
     
-    for j = 1:n
-      
-      % get the jth column in our training set
-      jth_col = X(:,j);
-      
-      % multiple each row in the pred_subtract_real with the value in the jth column 
-      % gets a mx1 matrix 
-      temp = pred_subtract_real .* jth_col;
-      
-      % get the sum of that m*1 matrix 
-      new_value = sum(temp);
-      
-      % pop this onto the bottom of descent_vals
-      descent_vals(j) = new_value;
-      
-      
-    end
+    % generate mxn+1 matrix with each column multiplied the values in pred_subtract_real
+    % each column will end up having the value for our theta
+    new_matrix = pred_subtract_real .* X;
     
-    % multiply descent_vals by the learning rate and simultaneously update theta 
-     theta = theta - alpha * (1/m) * descent_vals;
+    % calculate the sum of each column 
+    new_matrix = sum(new_matrix);
+    
+    % divide each column by alpha/m
+    new_matrix = (alpha/m) * new_matrix;
+    
+    % transpose this new matrix into column matrix form
+    new_matrix = new_matrix';
+    
+    % subtract the current thetas (n+1x1) with these new values (n+1x1)
+    theta = theta - new_matrix;
 
     % ============================================================
 
