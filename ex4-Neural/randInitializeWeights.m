9ea0059dfb0591b5a51e797a79c00fb79dbc19da
Kevin Faust <ikevinfaust@gmail.com>
2017-02-13 02:34:23 -0500

    Submitted Assignment Solutions

---
+++ ex4-Neural/randInitializeWeights.m
@@ -0,0 +1,27 @@
+function W = randInitializeWeights(L_in, L_out)
+%RANDINITIALIZEWEIGHTS Randomly initialize the weights of a layer with L_in
+%incoming connections and L_out outgoing connections
+%   W = RANDINITIALIZEWEIGHTS(L_in, L_out) randomly initializes the weights 
+%   of a layer with L_in incoming connections and L_out outgoing 
+%   connections. 
+%
+%   Note that W should be set to a matrix of size(L_out, 1 + L_in) as
+%   the first column of W handles the "bias" terms
+%
+
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Initialize W randomly so that we break the symmetry while
+%               training the neural network.
+%
+% Note: The first column of W corresponds to the parameters for the bias unit
+%
+
+% sqrt(6)/sqrt(num_input_features + num_second_layer_nodes)
+epsilon_init = 0.12;
+
+W = rand(L_out, ++L_in) * 2 * epsilon_init - epsilon_init;
+
+% =========================================================================
+
+end
